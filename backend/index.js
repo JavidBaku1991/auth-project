49f404d555e377da930e0bf508591d46edd2f999
Javid Baku <cavid-de-baku91@mail.ru>
2023-12-24 23:39:37 +0400

    latest

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,15 @@
 import express from 'express';
-
-const app =express();
-
-
+import mongoose from 'mongoose'
+import  dotenv from 'dotenv'
+dotenv.config();
+mongoose.connect(process.env.MONGO_URI,{
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    autoIndex: true,
+}).then(()=>console.log("MONGODB connected")).catch((err)=>console.log(err))
+const app=express();
 app.listen(3000,()=>{
     console.log('Server is running on port 3000')
 })
+
+
