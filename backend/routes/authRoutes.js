881154853fcf883b9b7c2c6aa9fa1c1bf91ca7e8
Javid Baku <cavid-de-baku91@mail.ru>
2023-12-26 16:59:08 +0400

    add redux

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,7 +1,8 @@
 import express  from "express";
 import User from "../models/user.js";
 import bcrypt from 'bcryptjs'
-import  errorHandler from "../utils/error.js";
+import errorHandler from '../utils/error.js';
+import  jwt from "jsonwebtoken";
 const router =express.Router();
 
 router.post('/signup', async (req,res,next)=>{
@@ -18,4 +19,23 @@ router.post('/signup', async (req,res,next)=>{
    }
  
 });
+
+router.post('/signin', async(req,res,next)=>{
+   const {email,password }=req.body;
+   try{
+      const validUser=await User.findOne({email});
+      if(!validUser) return next(errorHandler(404,'User not found!!!'));
+      const validPassword=bcrypt.compareSync(password,validUser.password)
+      if(!validPassword) return next(errorHandler(401,'Username or Password is wrong!!!'));
+   const token=jwt.sign({id:validUser._id},process.env.SECRET);
+     const {password:hashedPassword, ...rest}=validUser._doc;
+     const expireDate=new Date(Date.now()+36000);
+   res.cookie('access-token',token,{httpOnly:true,expires:expireDate}).status(200).json(rest)
+ 
+}   catch(err){
+      next(err)
+   }
+   })
+
+
 export default router;
